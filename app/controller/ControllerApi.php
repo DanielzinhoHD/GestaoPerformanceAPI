4e1d0755ab792ae4be38c9d8ddef9cb48d63a52c
Danielzinho <christiandanielgomes@gmail.com>
2022-11-12 00:43:32 -0300

    added content type json

--- app/controller/ControllerApi.php
+++ app/controller/ControllerApi.php
@@ -8,6 +8,8 @@ class ControllerApi extends ClassApi{
 
   public function __construct()
   {
+    header('Content-Type: application/json');
+
     echo json_encode(parent::retornaProjetos());
   }
 }
